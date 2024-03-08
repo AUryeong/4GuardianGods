96b4d53ade9edb225adff958da6dfb70f0107a8f
AUryeong <jykee7030@gmail.com>
2024-03-08 21:19:53 +0900

    Odin Inspector 에셋 추가

--- Assets/Plugins/FastScriptReload/Scripts/Editor/NewFields/NewFieldsRendererDefaultEditorPatch.cs
+++ Assets/Plugins/FastScriptReload/Scripts/Editor/NewFields/NewFieldsRendererDefaultEditorPatch.cs
@@ -29,12 +29,6 @@ namespace FastScriptReload.Editor.NewFields
                 var customEditorRenderingMethod = AccessTools.Method("UnityEditor.Editor:DrawDefaultInspector");
                 harmony.Patch(customEditorRenderingMethod, postfix: new HarmonyMethod(renderAdditionalFieldsDrawDefaultInspectorPostfix)); 
 
-#if ODIN_INSPECTOR
-                // Odin Inspector support
-                var renderAdditionalFieldsDrawOdinInspectorPostfix = AccessTools.Method(typeof(NewFieldsRendererDefaultEditorPatch), nameof(DrawOdinInspector));
-                var customOdinEditorRenderingMethod = AccessTools.Method("Sirenix.OdinInspector.Editor.OdinEditor:DrawOdinInspector");
-                harmony.Patch(customOdinEditorRenderingMethod, postfix: new HarmonyMethod(renderAdditionalFieldsDrawOdinInspectorPostfix));
-#endif
             }
         }
 
